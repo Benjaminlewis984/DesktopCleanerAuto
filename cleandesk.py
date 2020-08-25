e3347c09e7a09cafe49d403939be5be2fd340b17
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-08-25 13:20:30 -0700

    Created a function that sees downloaded files and puts them into a folder. If a file has the same name then that file will have an incremented extension label attached so that the file is not deleted

---
+++ cleandesk.py
@@ -0,0 +1,42 @@
+from watchdog.observers import Observer
+import time
+from watchdog.events import FileSystemEventHandler
+import os
+import json
+import shutil
+
+def handler_function(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            i = 1
+            if filename != "benjaminlewis":
+                new_name = filename
+                split_name = filename.split('.')
+                file_exists = os.path.isfile((folder_destination + '/' + new_name))
+                while file_exists:
+                    i += 1
+                    new_name = os.path.splitext(folder_to_track + '/' + new_name)[0] + str(i) + os.path.split('.')
+                    new_name = new_name.split("/")[4]
+                    file_exists = os.path.isfile(folder_destinationn + '/' + new_name)
+
+                src = folder_to_track + "/" + filename
+                new_name = folder_destination + "/" + new_name
+                os.rename(src, new_name)
+
+
+folder_to_track = '/Users/benjaminlewis/Desktop'
+folder_destination = '/Users/benjaminlewis/Desktop/jamin'
+event_handler = handler_function()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+
+
+
