e491a28d9a874e4b589da37cda2a4721477c9b3c
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-08-25 13:30:05 -0700

    Fixed error where the incremental label would not add the label to the os path

--- cleandesk.py
+++ cleandesk.py
@@ -8,16 +8,17 @@ import shutil
 def handler_function(FileSystemEventHandler):
     def on_modified(self, event):
         for filename in os.listdir(folder_to_track):
-            i = 1
+            inc_label = 1
             if filename != "benjaminlewis":
                 new_name = filename
                 split_name = filename.split('.')
                 file_exists = os.path.isfile((folder_destination + '/' + new_name))
                 while file_exists:
-                    i += 1
-                    new_name = os.path.splitext(folder_to_track + '/' + new_name)[0] + str(i) + os.path.split('.')
+                    inc_label += 1
+                    new_name = os.path.splitext(folder_to_track + '/' + new_name)[0] + str(inc_label) \
+                               + os.path.splitext(folder_to_track + '/' + new_name)[1]
                     new_name = new_name.split("/")[4]
-                    file_exists = os.path.isfile(folder_destinationn + '/' + new_name)
+                    file_exists = os.path.isfile(folder_destination + '/' + new_name)
 
                 src = folder_to_track + "/" + filename
                 new_name = folder_destination + "/" + new_name
