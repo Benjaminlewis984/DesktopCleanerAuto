2c1375f0af9de3aa6896f8b0c39ee53cf0a3f8f1
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-08-25 15:06:47 -0700

    Added extensions that will be recongnized and taken to there proper file locations. program is working and stable

--- cleandesk.py
+++ cleandesk.py
@@ -1,43 +1,24 @@
+#! /usr/bin/env python3
+
+from pathlib import Path
+from time import sleep
+
 from watchdog.observers import Observer
-import time
-from watchdog.events import FileSystemEventHandler
-import os
-import json
-import shutil
 
-def handler_function(FileSystemEventHandler):
-    def on_modified(self, event):
-        for filename in os.listdir(folder_to_track):
-            inc_label = 1
-            if filename != "benjaminlewis":
-                new_name = filename
-                split_name = filename.split('.')
-                file_exists = os.path.isfile((folder_destination + '/' + new_name))
-                while file_exists:
-                    inc_label += 1
-                    new_name = os.path.splitext(folder_to_track + '/' + new_name)[0] + str(inc_label) \
-                               + os.path.splitext(folder_to_track + '/' + new_name)[1]
-                    new_name = new_name.split("/")[4]
-                    file_exists = os.path.isfile(folder_destination + '/' + new_name)
+from eventhandler import EventHandler
 
-                src = folder_to_track + "/" + filename
-                new_name = folder_destination + "/" + new_name
-                os.rename(src, new_name)
+if __name__ == '__main__':
+    watch_path = Path.home() / 'Desktop'
+    destination_root = Path.home() / 'Desktop/benjaminlewis'
+    event_handler = EventHandler(watch_path=watch_path, destination_root=destination_root)
 
+    observer = Observer()
+    observer.schedule(event_handler, f'{watch_path}', recursive=True)
+    observer.start()
 
-folder_to_track = '/Users/benjaminlewis/Desktop'
-folder_destination = '/Users/benjaminlewis/Desktop/jamin'
-event_handler = handler_function()
-observer = Observer()
-observer.schedule(event_handler, folder_to_track, recursive=True)
-observer.start()
-
-try:
-    while True:
-        time.sleep(10)
-except KeyboardInterrupt:
-    observer.stop()
-observer.join()
-
-
-
+    try:
+        while True:
+            sleep(60)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
